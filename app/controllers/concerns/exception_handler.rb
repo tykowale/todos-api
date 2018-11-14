845be8a5cbae32b99d93ebb3c7b0b465d7c743a8
tykowale <tykowale@gmail.com>
2018-11-13 18:20:47 -0600

    start authenticating endpoints

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,13 +2,32 @@ module ExceptionHandler
 
   extend ActiveSupport::Concern
 
-  included do
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
-    end
+  class AuthenticationError < StandardError
+  end
+  class MissingToken < StandardError
+  end
+  class InvalidToken < StandardError
+  end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
-    end
+  included do
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ActiveRecord::RecordNotFound, with: :four_zero_four
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+  end
+
+  private
+
+  def four_twenty_two(e)
+    json_response({ message: e.message }, :unprocessable_entity)
+  end
+
+  def four_zero_four(e)
+    json_response({ message: e.message }, :not_found)
+  end
+
+  def unauthorized_request(e)
+    json_response({ message: e.message }, :unauthorized)
   end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :todos, foreign_key: :created_by
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
+
+  has_secure_password
+end
